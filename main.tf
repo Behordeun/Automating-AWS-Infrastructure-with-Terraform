6d3af6828041884743d908d2c06c7e599ec91a71
Muhammad Abiodun SULAIMAN <prince.behordeun@gmail.com>
2022-02-11 13:48:52 +0100

    First commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key="AKIA6N6COK6MBM42LZKG"
+    secret_key="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"
+}
+
+#references
+resource "aws_instance" "my-first-server" {
+    ami = "ami-04505e74c0741db8d"
+    instance_type = "t2.micro"
+    tags = {
+        name = "ubuntu"
+    }
+}
