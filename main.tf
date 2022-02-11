166db77dc5b14004cb3ea3a7b911eea7c076c00f
Muhammad Abiodun SULAIMAN <prince.behordeun@gmail.com>
2022-02-11 14:24:10 +0100

    Second commit

--- main.tf
+++ main.tf
@@ -9,6 +9,6 @@ resource "aws_instance" "my-first-server" {
     ami = "ami-04505e74c0741db8d"
     instance_type = "t2.micro"
     tags = {
-        name = "ubuntu"
+    #    name = "ubuntu"
     }
 }
