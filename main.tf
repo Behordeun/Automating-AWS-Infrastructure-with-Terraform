a11284b77347860e1e437686949a219a5e3ebeb7
Muhammad Abiodun SULAIMAN <prince.behordeun@gmail.com>
2022-02-12 21:01:56 +0100

    Created an AWS instance from Terraform

--- main.tf
+++ main.tf
@@ -1,23 +1,161 @@
 provider "aws" {
     region = "us-east-1"
-    access_key="AKIA6N6COK6MBM42LZKG"
-    secret_key="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"
+    access_key="AKIA6N6COK6MAHVAGIGJ"
+    secret_key="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"
 }
 
-#references
-resource "aws_vpc" "first-vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "production-vpc"
+# 1: Create a vpc
+
+resource"aws_vpc""prod-vpc" {
+  cidr_block="10.0.0.0/16"
+  tags={
+    Name = "production"
   }
 }
 
+# 2: Create Internet Gateway
 
-resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.first-vpc.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
 
   tags = {
+    Name = "internet_gateway"
+  }
+}
+
+# 3: Create a Custom Route Table
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Prod Route Table"
+  }
+}
+
+# 4: Create a Subnet
+
+resource"aws_subnet""subnet-1" {
+
+  vpc_id     =aws_vpc.prod-vpc.id
+
+  cidr_block="10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags={
+
     Name = "prod-subnet"
+
   }
+
+}
+
+# 5: Associate Subnet with Route Table
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# 6: Create Security Group to allow port 22, 80, 443
+
+resource "aws_security_group" "allow_web_traffic" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+   ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+   ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_web_traffic"
+  }
+}
+# 7: Create a network interface with an ip in the subnet that was created in step 4
+
+resource "aws_network_interface" "web_server_nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web_traffic.id]
+
+#  attachment {
+#    instance     = aws_instance.test.id
+#    device_index = 1
+#  }
+}
+
+# 8: Assign an elastic ip to the network interface created in step 7
+
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web_server_nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+# 9: Create Ubuntu server and install/enable apache2
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name      = "aws-main-key"
+
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.web_server_nic.id
+     }
+
+     user_data = <<-EOF
+                    #!/bin/bash
+                    sudo apt update -y
+                    sudo apt install apache2 =y
+                    sudo systemctl start apache2
+                    sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+                    EOF
+      tags= {
+        Name = "web_server"
+      }
 }
